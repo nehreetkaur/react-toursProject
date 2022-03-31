c9d004823846939ce7e13b4c15dd6604d358bc8d
Nehreet Kaur <kaur.nehreet@gmail.com>
2022-03-31 15:01:00 -0500

    code commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+import "./App.css";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+const[loading,setLoading]=useState(true);
+const [tours,setTours]=useState([]);
+
+const fetchTours= async()=>{
+  setLoading(true);
+ try{
+  const response= await fetch(url);
+  const tours=await response.json();
+  setLoading(false);
+  setTours(tours);
+  console.log(tours);
+
+ }catch(error){
+   setLoading(false);
+   console.log(error);
+ }
+
+
+ 
+  
+}
+
+
+useEffect(()=>{
+ fetchTours();
+}, [])
+
+ if(loading){
+   return (
+     <main>
+       <Loading/>
+     </main>
+   );
+ }
+
+
+  return (
+  
+  <main><Tours tours={tours}/>
+  </main>
+  );
+}
+
+export default App
